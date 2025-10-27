91176c4b0e98b59a80ac18ac72d06937c1a14ee0
tarang <rastogitarang5@gmail.com>
2025-10-27 20:30:07 +0530

    playing with components

---
+++ real-time-docs/src/app/documents/page.tsx
@@ -0,0 +1,9 @@
+const DocumentsPage = () => {
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <p>Documents Page</p>
+        </div>
+    );
+}
+
+export default DocumentsPage;
--- real-time-docs/src/app/page.tsx
+++ real-time-docs/src/app/page.tsx
@@ -1,10 +1,10 @@
-
-
+import Link from "next/link";
 const Home = () => {
 
   return (
+
     <div className="flex justify-center items-center min-h-screen">
-      <p>Hello World</p>
+      Click <Link href="/documents/123"> <span className="text-blue-500 underline">here</span></Link> to go to document id
     </div>
 
   );
